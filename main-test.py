d786b302c465a4487b163cd1fb82780c87908d44
zahudson95 <zahudson95@gmail.com>
2021-03-30 11:27:36 -0500

    added funcs and tests

--- main-test.py
+++ main-test.py
@@ -1,10 +1,32 @@
 # Import the Add function, and assert that it works correctly.
 from main import Add
+from main import Subtract
+from main import Multiply
+from main import Divide
 
 def TestAdd():
         assert Add(2,3) == 5
         assert Add(2,2) == 4
         print("Add Function works correctly")
 
+def TestSub():
+        assert Subtract(8,5) == 3
+        assert Subtract(14,5) == 9
+        print("Subtract Function works correctly")
+
+def TestMult():
+        assert Multiply(5,5) == 25
+        assert Multiply(10,3) == 30
+        print("Multiply Function works correctly")
+
+def TestDiv():
+        assert Divide(5,5) == 25
+        assert Divide(10,3) == 30
+        assert Divide(30,0) == None
+        print("Divide Function works correctly")
+
 if __name__ == '__main__':
         TestAdd()
+        TestSub()
+        TestMult()
+        TestDiv()
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 def Add(a, b):
         return a + b
-        
+
+def Subtract(a, b):
+        return a - b
+
+def Multiply(a, b):
+        return a * b
+
+def Divide(a, b):
+        if b > 0:
+                return a / b
+        else:
+                return None
+
+
 def SayHello():
-        print("sup world from srcmake")
+        print("Hello World!")
 
 if __name__ == '__main__':
         SayHello()
