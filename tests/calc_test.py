3b2c6975369155d1dadbfdb45dae9aa829155cb4
zahudson95 <zahudson95@gmail.com>
2021-04-01 09:39:22 -0500

    .

--- tests/calc_test.py
+++ tests/calc_test.py
@@ -1,16 +1,9 @@
 import sys
 
-from .context import calc
+import xmlrunner
+import unittest
 
-# Attempt to use back-ported unittest2 for Python 2.6 and earlier
-# However, it is strongly recommended to use Python 2.7 or 3.<latest>
-try:
-    if sys.version_info < (2, 7):
-        import unittest2
-    else:
-        raise ImportError()
-except ImportError:
-    import unittest
+from .context import calc
 
 NUMBER_1 = 3.0
 NUMBER_2 = 2.0
@@ -85,8 +78,6 @@ class CalculatorTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    import xmlrunner
-
     unittest.main(
         testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
         failfast=False,
